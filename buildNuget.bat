e554af6462452a346e63d3513dae8bd6d7e3d399
Artur Drobinskiy <artur.drobinskiy@rubius.com>
2017-06-07 10:59:33 +0700

    nuspecs

---
+++ buildNuget.bat
@@ -0,0 +1,3 @@
+%LOCALAPPDATA%\NuGet\NuGet.exe pack Realmius.Contracts\Realmius.Contracts.csproj -Build -properties Configuration=Release
+%LOCALAPPDATA%\NuGet\NuGet.exe pack Realmius\Realmius.csproj -Build -properties Configuration=Release
+%LOCALAPPDATA%\NuGet\NuGet.exe pack Realmius.Server\Realmius.Server.csproj -Build -properties Configuration=Release
