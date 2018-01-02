ffbffc21648f0ab5e59d5d9817b2231cc5aa1016
Artur Drobinskiy <artur.drobinskiy@gmail.com>
2018-01-02 23:30:44 +0700

    add migrations, moving forward to fix tests

---
+++ Realmius.Server/Infrastructure/DesignTimeDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Realmius.Server.Models;
+
+namespace Realmius.Server.Infrastructure
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<SyncStatusDbContext>
+    {
+        public SyncStatusDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<SyncStatusDbContext>();
+            builder.UseSqlServer(
+                "Server=localhost\\mssqllocaldb;Database=Realmius_Migrations;Integrated Security=true");
+
+            return new SyncStatusDbContext(builder.Options);
+        }
+    }
+}
